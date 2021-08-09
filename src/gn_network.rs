b94492077bb956d4cada7b61ac1837db3d28a6bb
Willi Kappler <willi.kappler@uni-tuebingen.de>
2021-08-09 15:26:47 +0200

    More elements for GNNetwork struct

--- src/gn_network.rs
+++ src/gn_network.rs
@@ -1,17 +1,26 @@
 
 use crate::gn_node::GNNode;
+
+use std::rc::Rc;
+
 pub struct GNNetwork {
-    input: Vec<f32>,
-    normal_nodes: Vec<GNNode>,
-    output_node: Vec<GNNode>,
+    pub(crate) input_values: Rc<Vec<f32>>,
+    pub(crate) normal_nodes: Vec<GNNode>,
+    pub(crate) normal_values: Vec<f32>,
+    pub(crate) output_node: Vec<GNNode>,
+    pub(crate) output_values: Vec<f32>,
+    pub(crate) expected_output: Rc<Vec<f32>>,
 }
 
 impl GNNetwork {
-    pub fn new() -> Self {
+    pub fn new(input_values: &Rc<Vec<f32>>, expected_output: &Rc<Vec<f32>>) -> Self {
         Self {
-            input: Vec::new(),
+            input_values: input_values.clone(),
             normal_nodes: Vec::new(),
+            normal_values: Vec::new(),
             output_node: Vec::new(),
+            output_values: Vec::new(),
+            expected_output: expected_output.clone(),
         }
     }
 }
