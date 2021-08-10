e22dc91ae1cadf807a24109bac664a9d26b25754
Willi Kappler <willi.kappler@uni-tuebingen.de>
2021-08-10 14:22:59 +0200

    Implement DWIndividual for GNNetwork, add trait GNDataProvider

--- src/gn_network.rs
+++ src/gn_network.rs
@@ -3,23 +3,27 @@ use crate::gn_node::GNNode;
 use crate::gn_output_node::GNOutputNode;
 
 use nanorand::{WyRand, Rng};
+use darwin_rs::{DWIndividual};
+
+pub trait GNDataProvider {
+    fn get_input_output(&self) -> Option<(&[f32], &[f32])>;
+}
 
 pub struct GNNetwork<T> {
     input_len: usize,
     output_len: usize,
     normal_nodes: Vec<GNNode>,
-    normal_values: Vec<f32>,
     output_nodes: Vec<GNOutputNode>,
     min_network_size: usize,
     max_network_size: usize,
     add_node_probability: f32,
     remove_node_probability: f32,
     calculation_steps: u8,
-    input_provider: T,
+    data_provider: T,
 }
 
-impl<'a, T: Iterator<Item=&'a [f32]>> GNNetwork<T> {
-    pub(crate) fn new(input_len: usize, output_len: usize, input_provider: T) -> Self {
+impl<T: GNDataProvider> GNNetwork<T> {
+    pub(crate) fn new(input_len: usize, output_len: usize, data_provider: T) -> Self {
         let mut output_nodes = Vec::new();
 
         for _ in 0..output_len {
@@ -27,25 +31,21 @@ impl<'a, T: Iterator<Item=&'a [f32]>> GNNetwork<T> {
         }
 
         let mut normal_nodes = Vec::new();
-        let mut normal_values = Vec::new();
 
         normal_nodes.push(GNNode::new());
-        normal_values.push(0.0);
         normal_nodes.push(GNNode::new());
-        normal_values.push(0.0);
 
         Self {
             input_len,
             output_len,
             normal_nodes,
-            normal_values,
             output_nodes,
             min_network_size: 2,
             max_network_size: 20,
             add_node_probability: 0.1,
             remove_node_probability: 0.1,
             calculation_steps: 2,
-            input_provider,
+            data_provider,
         }
     }
     fn add_node(&mut self) -> bool {
@@ -58,7 +58,6 @@ impl<'a, T: Iterator<Item=&'a [f32]>> GNNetwork<T> {
 
         if self.normal_nodes.len() < self.max_network_size {
             self.normal_nodes.push(GNNode::new());
-            self.normal_values.push(0.0);
             true
         } else {
             false
@@ -116,70 +115,83 @@ impl<'a, T: Iterator<Item=&'a [f32]>> GNNetwork<T> {
     fn roll_dice(&self, rng: &mut WyRand) -> f32 {
         (rng.generate::<u16>() as f32) / (u16::MAX as f32)
     }
-    pub(crate) fn mutate(&mut self) {
+    fn mutate(&mut self) {
         let mut rng = WyRand::new();
 
-        let operation = rng.generate_range(0_u8..3);
+        loop {
+            let operation = rng.generate_range(0_u8..3);
 
-        // TODO: use early return and move mutate_node to bottom
-
-        match operation {
-            0 => {
-                let dice = self.roll_dice(&mut rng);
-
-                if dice < self.add_node_probability {
-                    if self.add_node() {
-                        return
+            match operation {
+                0 => {
+                    let dice = self.roll_dice(&mut rng);
+    
+                    if dice < self.add_node_probability {
+                        if self.add_node() {
+                            break;
+                        }
                     }
                 }
-            }
-            1 => {
-                let dice = self.roll_dice(&mut rng);
-
-                if dice < self.remove_node_probability {
-                    if self.remove_node(&mut rng) {
-                        return
+                1 => {
+                    let dice = self.roll_dice(&mut rng);
+    
+                    if dice < self.remove_node_probability {
+                        if self.remove_node(&mut rng) {
+                            break;
+                        }
                     }
                 }
-            }
-            2 => {
-                // Mutate node, see below
-            }
-            _ => {
-                panic!("Unknown operation in GNNetwork::mutate: '{}'", operation);
+                2 => {
+                    self.mutate_node(&mut rng);
+                    break;
+                }
+                _ => {
+                    panic!("Unknown operation in GNNetwork::mutate: '{}'", operation);
+                }
             }
         }
 
-        self.mutate_node(&mut rng);
     }
-    fn calculate_single(&mut self, input_values: &[f32]) {
+    fn calculate_single(&self, input_values: &[f32], node_values: &mut [f32]) {
         for i in 0..self.normal_nodes.len() {
-            let result = self.normal_nodes[i].calculate_value(input_values, &self.normal_values);
-            self.normal_values[i] = result;
+            let result = self.normal_nodes[i].calculate_value(input_values, node_values);
+            node_values[i] = result;
         }
     }
-    pub(crate) fn calculate(&mut self, input_values: &[f32]) {
+    fn calculate(&self, input_values: &[f32], node_values: &mut [f32]) {
         for _ in 0..self.calculation_steps {
-            self.calculate_single(input_values);
+            self.calculate_single(input_values, node_values);
         }
     }
-    fn error_single(&self, expected_values: &[f32]) -> f32 {
+    fn error(&self, expected_values: &[f32], node_values: &[f32]) -> f32 {
         let mut error = 0.0;
 
         for i in 0..self.output_len {
-            let value = self.output_nodes[i].calculate_value(&self.normal_values);
+            let value = self.output_nodes[i].calculate_value(node_values);
             error += (value - expected_values[i]).abs();
         }
 
         error
     }
-    pub(crate) fn error(&mut self) -> f32 {
-        let mut error = 0.0;
+}
 
-        while let Some(data) = self.input_provider.next() {
-            error += self.error_single(data);
+impl<T: GNDataProvider> DWIndividual for GNNetwork<T> {
+    fn mutate(&mut self) {
+        self.mutate()
+    }
+
+    fn calculate_fitness(&self) -> f64 {
+        let mut error = 0.0;
+        let mut node_values= vec![0.0; self.normal_nodes.len()];
+
+        while let Some((input_values, expected_values)) = self.data_provider.get_input_output() {
+            self.calculate(input_values, &mut node_values);
+            error += self.error(expected_values, &node_values);
         }
 
-        error
+        error as f64
+    }
+
+    fn get_additional_fitness(&self) -> f64 {
+        self.normal_nodes.len() as f64
     }
 }
